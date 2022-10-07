89dc3c109447911920152cf830a49b3f828db954
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2022-10-07 13:55:56 -0300

    Added the React Chartjs 2 library, and defined Cards with the print of the data collected from the Api, as well as implementing the top 10 Chart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,23 +7,64 @@ import Card from "../components/Card"
 import Loading from "../components/Loading"
 import styles from "../styles/HomePage.module.sass"
 import ErrorDialog from "../components/ErrorDialog"
+import Top10ChartBarsPart from "../partials/Top10ChartBarsPart"
+import moment from "moment"
+import "moment/locale/pt-br"
+import WarningDialog from "../components/WarningDialog"
 
+// General Numbers
+const PrintNumbers = ({
+  deaths,
+  confirmed,
+  recovered,
+  labels
+}:{
+  deaths: number
+  confirmed: number
+  recovered: number
+  labels: string[]
+}) => {
+  if(typeof deaths === 'number' && typeof confirmed === 'number' && typeof recovered === 'number') {
+    return <div>
+      <h4 >{ labels[0] }: <strong>{ deaths.toLocaleString() }</strong></h4>
+      <h4 >{ labels[1] }: <strong>{ confirmed.toLocaleString() }</strong></h4>
+      <h4 >{ labels[2] }: <strong>{ recovered.toLocaleString() }</strong></h4>
+    </div>
+  } else {
+    return <ErrorDialog>
+      <h4>Erro!</h4>
+      <p>Dados inconsistentes.</p>
+    </ErrorDialog>
+  }
+}
+
+// Main
 let render: JSX.Element | JSX.Element[] = <Loading />
 export default function GlobalSummary() {
   const {global, setGlobal, top10Countries, setTop10Countries} = useContext(dataContext)
   const [countries, setCountries] = useState<ICountries[] | null>(null)
-  const [error, setError] = useState<Error | null>(null)
+  const [errorDataFetching, setErrorDataFetching] = useState<Error | null>(null)
+  const [errorInconsistentData, setErrorInconsistentData] = useState<Error | null>(null)
 
   useEffect(() => {
     if(global === null && top10Countries === null) {
       apiServiceGetSummary()
       .then(res => {
-        setGlobal({...res.Global})
-        setCountries([...res.Countries])
+        try {
+          if(res.Message === '') {
+            setGlobal({...res.Global})
+            setCountries([...res.Countries])
+          } else {
+            throw new Error(res.Message)
+          }
+        } catch (error) {
+          if(error instanceof Error) setErrorInconsistentData(error)
+        }
       })
-      .catch(e => setError(e))
+      .catch(e => setErrorDataFetching(e))
     }
-  }, [global, setGlobal, top10Countries])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     if(countries && top10Countries === null) {
@@ -33,27 +74,48 @@ export default function GlobalSummary() {
         .slice(0, 10)
       ])
     }
-  }, [countries, setTop10Countries, top10Countries])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [countries])
 
-  if(error) {
+  if(errorDataFetching) {
     render = <ErrorDialog>
-      <h4>{ error.name }</h4>
-      <p>{ error.message }</p>
+      <h4>Erro!</h4>
+      <p>Não foi possível conectar-se à base de dados, tente mais tarde.</p>
     </ErrorDialog>
   }
 
-  if(!error && global && top10Countries) {
-    console.log(global, top10Countries)
+  if(errorInconsistentData) {
+    render = <WarningDialog>
+      <h4>Aviso!</h4>
+      <p>Aguarde alguns instantes e em seguida atualize a página.</p>
+    </WarningDialog>
+  }
+
+  if(!errorDataFetching && !errorInconsistentData && global && top10Countries) {
     render = <section className={ styles['section'] }>
       <h1>Resumo Global</h1>
+      <h5>Atualizado { moment(global.Date).locale('pt-br').calendar() }</h5>
       <Card classAdd="shadow" cornerRad>
         <h3>Números Totais</h3>
+        <PrintNumbers
+          deaths={ global.TotalDeaths }
+          confirmed={ global.TotalConfirmed }
+          recovered={ global.TotalRecovered }
+          labels={['Mortos', 'Confirmados', 'Recuperados']}
+        />
       </Card>
       <Card classAdd="shadow" cornerRad>
         <h3>Números Parciais</h3>
+        <PrintNumbers
+          deaths={ global.NewDeaths }
+          confirmed={ global.NewConfirmed }
+          recovered={ global.NewRecovered }
+          labels={['Mortos', 'Confirmados', 'Recuperados']}
+        />
       </Card>
-      <Card classAdd="shadow" cornerRad>
+      <Card classAdd="shadow overflow-x" cornerRad>
         <h3>Top 10 Mortos</h3>
+        <Top10ChartBarsPart top10Countries={ top10Countries } />
       </Card>
     </section>
   }
---
+++ src/partials/Top10ChartBarsPart.tsx
@@ -0,0 +1,75 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+// My project
+import styles from "./Top10ChartBarsPart.module.sass"
+import { ICountry as ITop10Countries } from "../interface/summary"
+import ErrorDialog from '../components/ErrorDialog'
+import Loading from '../components/Loading'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+      display: false,
+    },
+    title: {
+      display: false,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+}
+
+export const data = {
+  labels: [''],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [0],
+      backgroundColor: '#592068',
+    }
+  ],
+}
+
+let render: JSX.Element | JSX.Element[] = <Loading />
+let chartLabels: string[] = ['']
+let chartData: number[] = [0]
+
+export default function Top10ChartBarsPart({ top10Countries }:{ top10Countries:ITop10Countries[] } ) {
+  chartLabels = ['']
+  chartData = [0]
+  if(top10Countries) {
+    chartLabels = [...top10Countries.map(el => el.Country)]
+    chartData = [...top10Countries.map(el => el.TotalDeaths)]
+    if(chartLabels && chartData) {
+      data.labels = chartLabels
+      data.datasets[0].data = chartData
+      render = <Bar options={options} data={data}/>
+    } else {
+      render = <ErrorDialog>
+        <h4>Erro!</h4>
+        <p>Dados inconsistentes.</p>
+      </ErrorDialog>
+    }
+  }
+  return <div className={ styles['box-chart'] }>
+    { render }
+  </div>
+}
