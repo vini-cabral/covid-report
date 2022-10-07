81d72697cdc4c9eef51d2ed9e27ff1bc52ce5b8d
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2022-10-07 08:19:06 -0300

    The data fetching implementation was refactored

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,62 @@
 import { useContext, useEffect, useState } from "react"
 // My Project
 import { apiServiceGetSummary } from "../client/service/apiService"
-import { ICountry as ITop10Countries } from "../interface/summary"
-import dataContext from "../context";
+import { ICountry as ICountries } from "../interface/summary"
+import dataContext from "../context"
+import Card from "../components/Card"
+import Loading from "../components/Loading"
+import styles from "../styles/HomePage.module.sass"
+import ErrorDialog from "../components/ErrorDialog"
 
+let render: JSX.Element | JSX.Element[] = <Loading />
 export default function GlobalSummary() {
   const {global, setGlobal, top10Countries, setTop10Countries} = useContext(dataContext)
+  const [countries, setCountries] = useState<ICountries[] | null>(null)
   const [error, setError] = useState<Error | null>(null)
 
   useEffect(() => {
     if(global === null && top10Countries === null) {
       apiServiceGetSummary()
       .then(res => {
-        setGlobal({
-          ...res.Global
-        })
-        setTop10Countries([
-          ...res.Countries = res.Countries
-          .sort((a: ITop10Countries, b: ITop10Countries) => b.TotalDeaths - a.TotalDeaths)
-          .slice(0, 10)
-        ])
+        setGlobal({...res.Global})
+        setCountries([...res.Countries])
       })
       .catch(e => setError(e))
     }
-  }, [global, setGlobal, setTop10Countries, top10Countries])
+  }, [global, setGlobal, top10Countries])
+
+  useEffect(() => {
+    if(countries && top10Countries === null) {
+      setTop10Countries([
+        ...countries
+        .sort((a: ICountries, b: ICountries) => b.TotalDeaths - a.TotalDeaths)
+        .slice(0, 10)
+      ])
+    }
+  }, [countries, setTop10Countries, top10Countries])
+
+  if(error) {
+    render = <ErrorDialog>
+      <h4>{ error.name }</h4>
+      <p>{ error.message }</p>
+    </ErrorDialog>
+  }
 
   if(!error && global && top10Countries) {
     console.log(global, top10Countries)
+    render = <section className={ styles['section'] }>
+      <h1>Resumo Global</h1>
+      <Card classAdd="shadow" cornerRad>
+        <h3>Números Totais</h3>
+      </Card>
+      <Card classAdd="shadow" cornerRad>
+        <h3>Números Parciais</h3>
+      </Card>
+      <Card classAdd="shadow" cornerRad>
+        <h3>Top 10 Mortos</h3>
+      </Card>
+    </section>
   }
 
-  return <h4>Summary</h4>
+  return render
 }
